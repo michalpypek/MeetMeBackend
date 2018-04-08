e3c4cb58c211ddf28cbb6ecf09797e1ceb00ce57
Kamil Kolesiński <kamil.kolesinski@gmail.com>
2018-04-08 17:32:19 +0200

    added rating, users controller

---
+++ MeetMe/MeetMe/Models/Rating.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetMe.Models
+{
+    public class Rating
+    {
+        
+
+        private int NumberOfRates { get; set; }
+        private int Sum { get; set; }
+
+        public  Rating()
+        {
+            Sum = 0;
+            NumberOfRates = 0;
+        }
+        public bool AddRating(int r)
+        {
+            if (r > 0 && r <= 5)
+            {
+                NumberOfRates++;
+                Sum += r;
+                return true;
+            }
+            return false;
+        }
+
+        public double GetRating()
+        {
+            return Sum / NumberOfRates;  
+        }
+
+        public bool NumberOfRatesGT3()
+        {
+            if (NumberOfRates > 3) return true;
+            return false;
+        }
+
+    }
+}
--- MeetMe/MeetMe/Models/User.cs
+++ MeetMe/MeetMe/Models/User.cs
@@ -3,23 +3,37 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace MeetMe.Models
 {
 	public class User
 	{
 		[Key]
-		public int id { get; set; }
+		public int id { get; set; }  
+        
+        [Required, StringLength(50)]
+        public string UserName { get; set; }
+		       
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
 
-		public string UserName { get; set; }
-		public string PhotoURL { get; set; }
-		public string Email { get; set; }
-		public string PhoneNumber { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
 
-		public float Rating { get; set; }
-		public string Description { get; set; }
-		public List<int> EventsCreatedIds { get; set; }
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        public string PhotoURL { get; set; }
+
+        public Rating Rating { get; set; }
+        public List<int> EventsCreatedIds { get; set; }
 		public List<int> EventsAttendingIds { get; set; }
 		public List<int> EventsAttendedIds { get; set; }
 	}
+    public class UserDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
 }
