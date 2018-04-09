1099a27076ee6e15e35247352e7423d12d26afaa
Kamil Kolesiński <kamil.kolesinski@gmail.com>
2018-04-09 17:22:27 +0200

    added Rating

--- MeetMe/MeetMe/Models/Event.cs
+++ MeetMe/MeetMe/Models/Event.cs
@@ -11,18 +11,18 @@ namespace MeetMe.Models
 		[Key]
 		public int id { get; set; }
 
-		public DateTime TimeCreated { get; set; }
-		public DateTime StartTime { get; set; }
-		public DateTime EndTime { get; set; }
+        public DateTime TimeCreated { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
 
-		public QrCode QrCode { get; set; }
+        public QrCode QrCode { get; set; }
 
-		public int CreatorId { get; set; }
-		public List<int> GuestsIds { get; set; }
-		public int GuestLimit { get; set; }
-		public AgeRestriction AgeRestriction { get; set; }
-		public EventType EventType { get; set; }
-		public float rating { get; set; }
-		public Location Location { get; set; }
-	}
+        public int CreatorId { get; set; }
+        public List<int> GuestsIds { get; set; }
+        public int GuestLimit { get; set; }
+        public AgeRestriction AgeRestriction { get; set; }
+        public EventType EventType { get; set; }
+        public Rating rating { get; set; }
+        public Location Location { get; set; }
+    }
 }
